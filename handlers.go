a74bcebc95c51e010957f5ea270b267fb44ce79f
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2015-11-11 13:37:44 -0800

    handler stubs added

--- handlers.go
+++ handlers.go
@@ -6,11 +6,22 @@ import (
 )
 
 func CustomersHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "sdsdf")
+	if r.Method == "POST" {
+		// TODO(danck) business logic
+	}
+
+	fmt.Fprint(w, http.StatusNotFound)
 }
 
 func OrdersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// TODO(danck) business logic
+	}
+	if r.Method == "PUT" {
+		// TODO(danck) business logic
+	}
 
+	fmt.Fprint(w, http.StatusNotFound)
 }
 
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
